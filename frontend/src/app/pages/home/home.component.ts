e822ae6aea53dd29b8c513dd63b0598a7c3d3fd6
Facundo Nicolas Fernandez <facufer00@gmail.com>
2025-12-11 02:30:24 -0300

    actualizacion home

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -1,51 +1,12 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { ProductService } from '../../services/product.service';
-import { Product } from '../../models/product.model';
-import { ProductListComponent } from '../../components/product-list/product-list.component';
+import { RouterModule } from '@angular/router'; // <--- IMPORTANTE
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, ProductListComponent, FormsModule],
+  imports: [CommonModule, RouterModule], // <--- IMPORTANTE
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
-export class HomeComponent implements OnInit {
-  private productService = inject(ProductService);
-
-  products: Product[] = [];
-  filteredProducts: Product[] = [];
-  searchTerm: string = '';
-
-  ngOnInit() {
-    this.loadProducts();
-  }
-
-  loadProducts() {
-    this.productService.getProducts().subscribe({
-      next: (data) => {
-        this.products = data.products || [];
-        this.filteredProducts = data.products || [];
-      },
-      error: (err) => console.error('Error cargando productos:', err),
-    });
-  }
-
-  filterProducts() {
-    if (!this.searchTerm) {
-      this.filteredProducts = this.products;
-      return;
-    }
-
-    const term = this.searchTerm.toLowerCase();
-
-    this.filteredProducts = this.products.filter(
-      (product) =>
-        // CORRECCIÓN: Usamos 'title' en lugar de 'name'
-        product.title.toLowerCase().includes(term) ||
-        product.description.toLowerCase().includes(term)
-    );
-  }
-}
+export class HomeComponent {}
